7341e56ea199397b7cf2beb7ad4eab8c53e8c0ce
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-29 07:54:02 -0800

    Home page route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * The method Response.render() is used to render a specified template along with the values of 
+ * named variables passed in an object, and then send the result as a response.
+ */
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
